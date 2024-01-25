f2a28b1181325a36daf7d505c2e7391f72f1d9ee
Heverton <hb.hevertonborges@gmail.com>
2024-01-25 10:34:28 -0300

    finish router

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,9 @@
-import { Router } from "express";
-import { createUser, findUser } from "./controllers";
+import { Router, Request, Response } from "express";
+import { createUser, deleteUser, findUser, updateUser } from "./controllers";
 
+export const router = Router;
 
-export const routes = Router;
-
-routes.post("/", createUser)
-routes.get("/", findUser)
-
-
+router.post("/", (req: Request, res: Response) => createUser(req.body, res));
+router.get("/:id", (req: Request, res:Response) => findUser(req.query.id, res));
+router.put("/:id", (req: Request, res: Response) => updateUser(req.query.id, req.body res))
+router.delete("/:id", (req: Request, res: Response) => deleteUser(req.query.id, res))
