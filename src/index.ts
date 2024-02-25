8655eda5de0198ffe8c281a39d9929e4cdb96402
Heverton <hb.hevertonborges@gmail.com>
2024-02-25 16:01:38 -0300

    fixing some bugs

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 import express, { Express } from "express";
-import cors from "cors";
 import { router } from "./routes";
 
 const port = process.env.PORT || 8080;
 
-const app: Express = express().use(cors()).use(express.json()).use("/", router);
+const app: Express = express().use(express.json()).use("/", router);
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
